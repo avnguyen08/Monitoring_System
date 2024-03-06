51d72526402458db11f0fa6524b39feb1412da9b
Test <avnguyen08@gmail.com>
2024-03-06 10:49:54 -0800

    readme updated with picture

--- Version_B/Flash-Version-C1_04/flash.bat
+++
@@ -1,2 +0,0 @@
-"esptool.exe" --chip esp32s3 --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 "Monitoring_System_LCD.ino.bootloader.bin" 0x8000 "Monitoring_System_LCD.ino.partitions.bin" 0xe000 "boot_app0.bin" 0x10000 "Monitoring_System_LCD.ino.bin" 
-pause
