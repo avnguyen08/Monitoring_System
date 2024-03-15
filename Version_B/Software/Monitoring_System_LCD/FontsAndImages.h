e50c8d0a42a166993ff16ac7dd9ae16ee5e4bf2d
Aaron <avnguyen08@gmail.com>
2024-03-15 16:50:56 -0700

    gui update

--- Version_B/Software/Monitoring_System_LCD/FontsAndImages.h
+++ Version_B/Software/Monitoring_System_LCD/FontsAndImages.h
@@ -1,14 +1,18 @@
 #include "AllMyFonts/MyFont.h"
 #include "AllMyFonts/MyriadProBold58.h"
 #include "AllMyFonts/MyriadProBold70.h"
+#include "AllMyFonts/MyriadProBold65.h"
 #include "AllMyFonts/MyriadProBold25.h"
+#include "AllMyFonts/Arimo_Bold_70.h"
 #include "Images/Artboard_1.h"   //Contains SSS bit map array for Solid State Systems logo image
 
 // Easily remembered name for the font
 #define MYFONT25 &MyriadProBold25pt7b
 #define MYFONT58 &MyriadProBold58pt7b
+#define MYFONT65 &MyriadProBold65pt7b
 #define MYFONT70 &MyriadProBold70pt7b
+#define ARIMO70 &Arimo_Bold_70
 
 #define AMPFONT MYFONT58
 #define TIMEFONT MYFONT25
-#define AMPFONT70 MYFONT70
+#define AMPFONT70 MYFONT65
